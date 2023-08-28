e3f0e0df6f7c1dac8772f283989c3e398ce9b7bc
evejin9 <evejin9@naver.com>
2023-08-28 17:49:22 +0900

    2023-08-28 Sightseeing tours
    - 서브 메뉴 구현
    - 서브 페이지 라우팅
    - 서브 페이지 구현

---
+++ src/pages/SightseeingTours/ClassicTour.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import TourItem from '../../components/TourItem';
+import styled from 'styled-components';
+
+import toursData from "../../data/sightseeingToursData.json";
+
+const ClassicTourStyle = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+`
+
+
+function ClassicTour(props) {
+
+  return (
+    <ClassicTourStyle>
+      {
+        toursData.map((tour) => {
+          if (tour.classicTours) {
+            return tour.classicTours.map((a) => {
+              return <TourItem key={a.id} title={a.name} photo={a.imagePath} />
+            })
+          }
+        })
+      }
+    </ClassicTourStyle>
+  );
+}
+
+export default ClassicTour;
