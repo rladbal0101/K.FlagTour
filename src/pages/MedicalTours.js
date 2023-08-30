1fb97d68fd9d924cd2385a4d7fcc78e39a826913
rladbal0101 <rladbal0101@naver.com>
2023-08-30 16:24:36 +0900

    2023-08-30 Medical tours
    Medical tours 데이터파일로 변경

--- src/pages/MedicalTours.js
+++ src/pages/MedicalTours.js
@@ -1,27 +1,27 @@
 import React from 'react';
 import styled from 'styled-components';
 import SubPageStyle from '../components/SubPageStyle';
-// import { Outlet } from 'react-router';
 
 import subPageImg from "../data/subPageImgData.json";
+import medicalTours from "../data/medicalToursData.json";
 
 const MedicalStyled = styled.div`
   margin: 0 150px 80px 150px;
-`
+`;
 
 const MedicalSection = styled.div`
   margin-top: 50px;
 
-  span {
+  .title {
     font-weight: 700;
   }
 
   ul {
     margin-top: 20px;
-
-    li + li {
-      margin-top: 20px;
-    }
+    list-style-type: '- ';
+    list-style-position: inside;
+    padding-left: 10px;
+    text-indent: -10px;
   }
 `;
 
@@ -42,24 +42,19 @@ function MedicalTours(props) {
         <p>
           K.Flag Tour provides wide range of medical tourism services. It includes body check-ups, treatment of serious diseases and plastic surgery.
         </p>
-        <MedicalSection>
-          <span>Body check-ups </span>may be very diverse, depending on complexity of tests. Most common programs are:
-
-          <ul>
-            <li>
-              Basic Program: designed for adults for the purpose of preventing adult related diseases and diagnosing them in their early stage. Required test time is about 2 hours, some results are ready on the same day, some will be sent our later.
-            </li>
-            <li>
-              Premium program: usually most extensive and intensive health screening program, the patient gets complete check-up and accurate early diagnosis of various diseases by general body CT scan, MRI or PET, and other specific tests. Required time usually is about 6 hours and all results are ready in couple weeks.
-            </li>
-          </ul>
-        </MedicalSection>
-        <MedicalSection>
-          <span>Medical treatment</span> for existing diseases is available for:
-
-
-        </MedicalSection>
-
+        {
+          medicalTours.map(tour => 
+            <MedicalSection>
+              <span className='title'>{tour.title}</span>
+              <span> {tour.description}</span>
+              <ul>
+                {tour.contents.map(content =>
+                  <li>{content}</li>
+                )}
+              </ul>
+            </MedicalSection>
+          )
+        }
       </MedicalStyled>
     </>
   );
