3a341f107723a2489d24e86e7a89cd6af0b6d543
rladbal0101 <rladbal0101@naver.com>
2023-09-05 17:19:12 +0900

    2023-09-05 Medical Tour 스타일 변경
    Medical Tour 리스트 스타일 변경

--- src/pages/MedicalTours.js
+++ src/pages/MedicalTours.js
@@ -4,25 +4,10 @@ import SubPageStyle from '../components/SubPageStyle';
 
 import subPageImg from "../data/subPageImgData.json";
 import medicalTours from "../data/medicalToursData.json";
+import MedicalTourUlStyle from '../components/MedicalTourUlStyle';
 
 const MedicalStyled = styled.div`
   margin: 0 150px 80px 150px;
-`;
-
-const MedicalSection = styled.div`
-  margin-top: 50px;
-
-  .title {
-    font-weight: 700;
-  }
-
-  ul {
-    margin-top: 20px;
-    list-style-type: '- ';
-    list-style-position: inside;
-    padding-left: 10px;
-    text-indent: -10px;
-  }
 `;
 
 function MedicalTours(props) {
@@ -43,16 +28,13 @@ function MedicalTours(props) {
           K.Flag Tour provides wide range of medical tourism services. It includes body check-ups, treatment of serious diseases and plastic surgery.
         </p>
         {
-          medicalTours.map(tour => 
-            <MedicalSection>
-              <span className='title'>{tour.title}</span>
-              <span> {tour.description}</span>
-              <ul>
-                {tour.contents.map(content =>
-                  <li>{content}</li>
-                )}
-              </ul>
-            </MedicalSection>
+          medicalTours.map(tour =>
+            <MedicalTourUlStyle
+              title={tour.title}
+              description={tour.description}
+              contents={tour.contents}
+              style={tour.style}
+            />
           )
         }
       </MedicalStyled>
